cd57c3b8f9f3ecd85a3b516d18f6d48b66678d49
Clarence289 <sitholeclarence9@gmail.com>
2023-11-20 16:32:02 +0200

    first commit

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,7 +1,27 @@
 import React from 'react';
+//components
+import CheckIn from '../components/CheckIn';
+import AdultsDropdown from '../components/AdultsDropdown';
+import KidsDropdown from '../components/AdultsDropdown';
+import CheckOut from '../components/CheckOut';
 
 const BookForm = () => {
-  return <div>BookForm</div>;
+  return <form className='h-[300p]w-full lg:h-[70px]'>
+    <div className='flex flex-col w-full h-full lg:flex-row'>
+    <div className='flex-1 border-r'>
+      <CheckIn/>
+    </div>
+    <div className='flex-1 border-r'>
+      <CheckOut/>
+    </div>
+    <div className='flex-1 border-r'>
+    <AdultsDropdown/>
+    </div>
+    <div  className='flex-1 border-r'>
+    <KidsDropdown/>
+    </div>
+  </div>
+</form>
 };
 
 export default BookForm;
