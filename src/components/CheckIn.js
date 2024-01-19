2fd070bfb758d1fd9566b4c9d0b76edfcb9fd9c6
Clarence289 <sitholeclarence9@gmail>
2024-01-19 15:44:51 +0200

    Initial commit

--- src/components/CheckIn.js
+++ src/components/CheckIn.js
@@ -1,25 +1,33 @@
 import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-import '../datepicker.css';
 import { BsCalendar } from 'react-icons/bs';
+import { debounce } from 'lodash';
 
 const CheckIn = () => {
-  const [startDate, setStartDate] = useState(false);
+  const [startDate, setStartDate] = useState(null);
+
+  const handleIconClick = debounce(() => {
+    // Your logic when the icon is clicked
+    console.log('Icon clicked!');
+  }, 300); // Adjust the delay as needed
 
   return (
     <div className='relative flex items-center justify-end h-full'>
-      {/*icon*/}
-      <div className='absolute z-10 pr-8'>
+      {/* clickable icon */}
+      <div
+        className='absolute z-10 pr-8 cursor-pointer'
+        onClick={handleIconClick}
+      >
         <div>
-    <BsCalendar className='text-accent text-base'/>
+          <BsCalendar className='text-accent text-base' />
         </div>
       </div>
       <DatePicker
         className='w-full h-full'
         selected={startDate}
         placeholderText='Check in'
-        onChange={date => setStartDate(date)}
+        onChange={(date) => setStartDate(date)}
       />
     </div>
   );
--- src/components/CheckOut.js
+++ src/components/CheckOut.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import '../datepicker.css';
@@ -7,23 +7,36 @@ import { BsCalendar } from 'react-icons/bs';
 const CheckOut = () => {
   const [endDate, setEndDate] = useState(null);
 
+  const handleDatePickerClick = () => {
+    // Your logic when the date picker is clicked
+    console.log('CheckOut date picker clicked!');
+  };
+
   return (
-    <div className='relative flex items-center justify-end h-full'>
-      {/*icon*/}
+    <div
+      className='relative flex items-center justify-end h-full cursor-pointer'
+      onClick={handleDatePickerClick}
+    >
+      {/* icon */}
       <div className='absolute z-10 pr-8'>
         <div>
-    <BsCalendar className='text-accent text-base'/>
+          <BsCalendar className='text-accent text-base' />
         </div>
       </div>
       <DatePicker
         className='w-full h-full'
         selected={endDate}
         placeholderText='Check out'
-        onChange={date => setEndDate(date)}
+        onChange={(date) => setEndDate(date)}
+        onClickOutside={() => console.log('Clicked outside CheckOut date picker')}
+        popperModifiers={{
+          preventOverflow: {
+            enabled: true,
+          },
+        }}
       />
     </div>
   );
 };
-
 
 export default CheckOut;
